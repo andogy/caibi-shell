ee0d38d58f27014998cd93d10f0c198e4ddddc47
andogy <76235521+andogy@users.noreply.github.com>
2021-03-14 13:02:06 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,15 +4,7 @@ echo " "
 echo "请尽量使用顶级域名"
 echo "注意，本测压工具不对后果进行任何承担"
 echo "本测压工具可能会让网站造成超时无法连接"
-echo "如果你怕了，请输入e字样，愿意承担后果输入o"
-read -n1 -p "你要继续吗？[e/o]" eo
-if ("$eo" == "e")
-then
-      echo "好吧，也许只是你手贱点进来，傻逼"
-      exit
-elif ("$so" == "o")
-then
-    echo " "
+echo " "
 echo "请输入测试对象网址:"
 read url
 echo "请输入每秒发包数:"
@@ -29,7 +21,3 @@ echo "总发包数: ${b}"
 echo " "
 echo "测压完毕后将会自动生成测压报告"
 ab -n ${b} -c ${a} ${url} 
-else
-    echo "错误啦，乱输东西啊，垃圾东西，用个屁啊！"
-exit
-fi
