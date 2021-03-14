c28a141304055bb82883bd04a449fd3415713e55
andogy <76235521+andogy@users.noreply.github.com>
2021-03-14 23:59:53 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,18 +9,21 @@ echo "本测压工具可能会让网站造成超时无法连接"
 echo " "
 echo "请输入测试对象网址:"
 read url
-echo "请输入每秒发包数:"
+echo "请输入每次访问数:"
 read a
-echo "请输入总发包数:"
+echo "请输入总压测次数:"
 read b
+echo "请输入压测秒数（不低于5）:"
+read c
 echo "已开始进行测压"
 echo " "
 echo "本脚本只用于学习研究用途，对于轰炸他人站点，后果自负"
 echo "测压详情: "
 echo "对象: ${url}"
-echo "每秒发包数: ${a}"
-echo "总发包数: ${b}"
+echo "每次访问数: ${a}"
+echo "总压测次数: ${b}"
+echo "测压秒数: $c"
 echo " "
 echo "测压完毕后将会自动生成测压报告"
 pkg install apache2
-ab -n ${b} -c ${a} ${url} 
+ab -n ${b} -c ${a} -T ${c} -r ${url} 
